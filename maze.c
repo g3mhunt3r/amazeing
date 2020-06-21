ba87def06ea225252d29a73ffd8448c842880586
g3mhunt3r <rgopikrishnan28@gmail.com>
2020-06-21 13:00:05 +0530

    Refactor part 2

--- maze.c
+++ maze.c
@@ -2,7 +2,7 @@
 #include<locale.h>
 int main ()
 {
-	int i;
+	int i,x;
 	char c;
 	setlocale(LC_ALL, "");
 	initscr();
@@ -12,37 +12,17 @@ int main ()
         init_pair(3, COLOR_GREEN, COLOR_BLACK);
         init_pair(4, COLOR_CYAN, COLOR_BLACK);
 	noecho();
+	char arr[4][4]={"↑","↓","→","←"};
 	while(c!='q')
 	{
 		c = getch();
 		if(c==0x1b)
-		{	
+		{
 			getch();
-			switch(getch())
-			{
-				case 0x41:
-					attron(COLOR_PAIR(1));
-					mvprintw(10,10,"↑");
-					attroff(COLOR_PAIR(1));
-					break;
-				case 0x42:
-					attron(COLOR_PAIR(2));
-					mvprintw(10,10,"↓");
-					attroff(COLOR_PAIR(2));
-					break;
-				case 0x43:
-					attron(COLOR_PAIR(3));
-					mvprintw(10,10,"→");
-					attroff(COLOR_PAIR(3));
-					break;
-				case 0x44:
-					attron(COLOR_PAIR(4));
-					mvprintw(10,10,"←");
-					attroff(COLOR_PAIR(4));
-					break;
-				default:
-					mvprintw(10,10,".");
-			}
+			x=getch()%0x41;
+			attron(COLOR_PAIR(x+1));
+			mvprintw(10,10,arr[x]);
+			attroff(COLOR_PAIR(x+1));
 		}
 		else
 			mvprintw(10,10,"%c",c);
