086b3f7fe54d12ad69b2787fcc0649be61f8cebd
g3mhunt3r <rgopikrishnan28@gmail.com>
2020-06-21 13:30:52 +0530

    Center text and allow resize

--- maze.c
+++ maze.c
@@ -2,7 +2,7 @@
 #include<locale.h>
 int main ()
 {
-	int i,x;
+	int i,x,row,col;
 	char c;
 	setlocale(LC_ALL, "");
 	initscr();
@@ -13,19 +13,22 @@ int main ()
         init_pair(4, COLOR_CYAN, COLOR_BLACK);
 	noecho();
 	char arr[4][4]={"↑","↓","→","←"};
+	
 	while(c!='q')
 	{
 		c = getch();
+		clear();
+		getmaxyx(stdscr,row,col);
 		if(c==0x1b)
 		{
 			getch();
 			x=getch()%0x41;
 			attron(COLOR_PAIR(x+1));
-			mvprintw(10,10,arr[x]);
+			mvprintw(row/2,col/2,arr[x]);
 			attroff(COLOR_PAIR(x+1));
 		}
 		else
-			mvprintw(10,10,"%c",c);
+			mvprintw(row/2,col/2,"%c",c);
 		refresh();
 	}
 	endwin();
