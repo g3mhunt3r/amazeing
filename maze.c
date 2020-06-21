3ad0827434423ee89dce4bded8559e4745e35e6c
g3mhunt3r <rgopikrishnan28@gmail.com>
2020-06-21 11:43:50 +0530

    Coloured arrows

--- maze.c
+++ maze.c
@@ -1,9 +1,16 @@
 #include<curses.h>
+#include<locale.h>
 int main ()
 {
 	int i;
 	char c;
+	setlocale(LC_ALL, "");
 	initscr();
+	start_color();
+        init_pair(1, COLOR_RED, COLOR_BLACK);
+        init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+        init_pair(3, COLOR_GREEN, COLOR_BLACK);
+        init_pair(4, COLOR_CYAN, COLOR_BLACK);
 	noecho();
 	while(c!='q')
 	{
@@ -15,24 +22,34 @@ int main ()
 				switch(getch())
 				{
 					case 0x41:
-						printf("↑");
+						attron(COLOR_PAIR(1));
+						printw("↑");
+						attroff(COLOR_PAIR(1));
 						break;
 					case 0x42:
-						printf("↓");
+						attron(COLOR_PAIR(2));
+						printw("↓");
+						attroff(COLOR_PAIR(2));
 						break;
 					case 0x43:
-						printf("→");
+						attron(COLOR_PAIR(3));
+						printw("→");
+						attroff(COLOR_PAIR(3));
 						break;
 					case 0x44:
-						printf("←");
+						attron(COLOR_PAIR(4));
+						printw("←");
+						attroff(COLOR_PAIR(4));
 						break;
 					default:
-						printf(".");
+						printw(".");
 				}
 			}
 		}
-		//printf("\nChar: %x",c);
-		fflush(stdout);
+		else
+			printw("%c",c);
+		printw("\n");
+		refresh();
 	}
 	endwin();
 	return 0;
