f59f9d13bc4433d9f3b4d540b0c0120533b8810b
g3mhunt3r <rgopikrishnan28@gmail.com>
2020-06-28 21:31:15 +0530

    Added player and basic movement

--- maze.c
+++ maze.c
@@ -19,6 +19,7 @@ void readmaze()
 }
 void printmaze()
 {
+	move(0,0);
 	int flag=1;
 	for(int i=0;i<mx;i++)
 	{
@@ -37,6 +38,88 @@ void printmaze()
 		printw("\n");
 	}
 }
+void up()
+{
+	for(int i=1;i<mx;i++)
+	{
+		for(int j=0;j<my;j++)
+		{
+			if(maz[i][j]=='A')
+			{
+				if(maz[i-1][j]!='X')
+				{
+					maz[i-1][j]='A';
+					maz[i][j]=' ';
+					printmaze();
+					return;
+				}
+			}
+		}
+	}
+}
+
+void down()
+{
+	for(int i=0;i<mx-1;i++)
+	{
+		for(int j=0;j<my;j++)
+		{
+			if(maz[i][j]=='A')
+			{
+				if(maz[i+1][j]!='X')
+				{
+					maz[i+1][j]='A';
+					maz[i][j]=' ';
+					printmaze();
+					return;
+				}
+			}
+		}
+	}
+}
+void right()
+{
+	for(int i=0;i<mx;i++)
+	{
+		for(int j=0;j<my-1;j++)
+		{
+			if(maz[i][j]=='A')
+			{
+				if(maz[i][j+1]!='X')
+				{
+					maz[i][j+1]='A';
+					maz[i][j]=' ';
+					printmaze();
+					return;
+				}
+			}
+		}
+	}
+}
+void left()
+{
+	for(int i=0;i<mx;i++)
+	{
+		for(int j=1;j<my;j++)
+		{
+			if(maz[i][j]=='A')
+			{
+				if(maz[i][j-1]!='X')
+				{
+					maz[i][j-1]='A';
+					maz[i][j]=' ';
+					printmaze();
+					return;
+				}
+			}
+		}
+	}
+}
+/*
+void printplayer()
+{
+	13,29
+}*/
 int main()
 {
 	readmaze();
@@ -55,7 +138,6 @@ int main()
 	while(c!='q')
 	{
 		c = getch();
-		clear();
 		getmaxyx(stdscr,row,col);
 		if(c==0x1b)
 		{
@@ -64,10 +146,17 @@ int main()
 			attron(COLOR_PAIR(x+1));
 			mvprintw(row/2,col/2,arr[x]);
 			attroff(COLOR_PAIR(x+1));
+			if(x==0)
+				up();
+			else if(x==1)
+				down();
+			else if(x==2)
+				right();
+			else if(x==3)
+				left();
 		}
 		else
 			mvprintw(row/2,col/2,"%c",c);
-		refresh();
 	}
 	endwin();
 	return 0;
