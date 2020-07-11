f35ced2cef5155e9ae56f3a3666df9b5cbfb46f5
g3mhunt3r <rgopikrishnan28@gmail.com>
2020-07-11 19:33:29 +0530

    comments and minor optimization

--- maze.c
+++ maze.c
@@ -1,50 +1,59 @@
 #include<curses.h>
 #include<locale.h>
 #include<stdlib.h>
-#define player "⍾"
-int mx,my;
-char **maz;
+#define player "⍾"	// replacing player with ⍾
+int mx,my;			// number of rows and columns of maze
+char **maz;			// pointer to character array storing maze 
+
+/*******
+reads the maze from the file maze.txt into maz
+*******/
 void readmaze()
 {
-	FILE *mazep;
-	mazep=fopen("maze.txt","r");
-	fscanf(mazep,"%d %d\n",&mx,&my);
-	maz=malloc(mx*sizeof(char*));
-	for(int i=0;i<mx;i++)
-        maz[i]=malloc(my*sizeof(char));
-	for(int i=0;i<mx;i++)
+	FILE *mazep;						// creating file pointer 
+	mazep=fopen("maze.txt","r");		// opning file in read mode
+	fscanf(mazep,"%d %d\n",&mx,&my);	// read no. of rows and columns from file
+	maz=malloc(mx*sizeof(char*));		// allocating space for row pointers
+	for(int i=0;i<mx;i++)				// for each row: 
+        maz[i]=malloc(my*sizeof(char));	// allocate space for row		
+	for(int i=0;i<mx;i++)				// for each row:
 	{
-		fgets(maz[i],my,mazep);
-		fgetc(mazep);
+		fgets(maz[i],my,mazep);			// reads row into maz[i]
+		fgetc(mazep);					// reads newline character
 	}
 }
+
+/*******
+displays maze and player character
+*******/
 void printmaze()
 {
-	move(0,0);
-	int flag=1;
-	for(int i=0;i<mx;i++)
+	move(0,0);								// move the cursor to (0,0)
+	int flag=1;								// used to alternate bright/dark characters of maze
+	for(int i=0;i<mx;i++)					// for each row:
 	{
-		for(int j=0;j<my;j++)
+		for(int j=0;j<my;j++)				// for each element in row:
 		{
-			if(maz[i][j]=='X')
+			if(maz[i][j]=='X')				// if wall:
 			{
-				attron(COLOR_PAIR(4));
-				printw(flag?"█":"▒");
-				attroff(COLOR_PAIR(4));
+				attron(COLOR_PAIR(4));		// eanble color pair 4
+				printw(flag?"█":"▒");		// print alternating maze block
+				attroff(COLOR_PAIR(4));		// disable color pair 4
 			}
-			else
+			else							// if not wall:
 			{
-				if(maz[i][j]=='A')
-				{	attron(COLOR_PAIR(2));
+				if(maz[i][j]=='A')			// if player:
+				{							// print player character
+					attron(COLOR_PAIR(2));
 					printw(player);
 					attroff(COLOR_PAIR(2));
 				}
-				else
-					printw("%c",maz[i][j]);
+				else						// if any other character:
+					printw("%c",maz[i][j]);	// print character
 			}
-			flag=!flag;
+			flag=!flag;						// toggle bring/dark for next block
 		}
-		printw("\n");
+		printw("\n");						// end of row
 	}
 }
 void up()
@@ -124,49 +133,39 @@ void left()
 		}
 	}
 }
-/*
-void printplayer()
-{
-	13,29
-}*/
+
 int main()
 {
 	readmaze();
 	int i,x,row,col;
-	char c;
-	setlocale(LC_ALL, "");
-	initscr();
-	start_color();
-        init_pair(1, COLOR_RED, COLOR_BLACK);
-        init_pair(2, COLOR_YELLOW, COLOR_BLACK);
-        init_pair(3, COLOR_GREEN, COLOR_BLACK);
-        init_pair(4, COLOR_CYAN, COLOR_BLACK);
-    noecho();
-	char arr[4][4]={"↑","↓","→","←"};
+	void (*move[])()={up,down,right,left};
+	char c;											// input buffer
+	setlocale(LC_ALL, "");							// required for rendering wide characters
+	initscr();										// initialise ncurses screen
+	start_color();									// initialise colors
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(3, COLOR_GREEN, COLOR_BLACK);
+	init_pair(4, COLOR_CYAN, COLOR_BLACK); 
+	noecho();										// disable echo on input
+	char arr[4][4]={"↑","↓","→","←"};				// array of directional characters
 	printmaze();
-	while(c!='q')
+	while(c!='q')									// continuously read input
 	{
-		c = getch();
-		getmaxyx(stdscr,row,col);
-		if(c==0x1b)
+		c = getch();								// read a byte
+		getmaxyx(stdscr,row,col);					// get dimensions of terminal
+		if(c==0x1b)									// if start of special character sequence:
 		{
-			getch();
-			x=getch()%0x41;
+			getch();								// drop next char from buffer
+			x=getch()%0x41;							// read last byte from buffer
 			attron(COLOR_PAIR(x+1));
-			mvprintw(row/2,col/2,arr[x]);
+			mvprintw(row/2,col/2,arr[x]);			// print corresponding arrow at centre
 			attroff(COLOR_PAIR(x+1));
-			if(x==0)
-				up();
-			else if(x==1)
-				down();
-			else if(x==2)
-				right();
-			else if(x==3)
-				left();
+			(*move[x])();							// call corresponding move function
 		}
 		else
-			mvprintw(row/2,col/2,"%c",c);
+			mvprintw(row/2,col/2,"%c",c);			// print any other character entered
 	}
-	endwin();
+	endwin();										// restore terminal
 	return 0;
 }
